6a995bc8c779633f9d1873d79d0136cc3923d772
Sabrina Singh <sabrinasingh@Sabrinas-MacBook-Pro.local>
2020-09-09 08:32:36 -0400

    r

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
     <Banner></Banner>
     <img src = { nucalm } alt = "NuCalm" className = "nucalm-img-home"/>
     {/* <h1  className="hero">Welcome to the NuCalm Beta-Testing Site!</h1> */}
-    <h3 className="subHero">Welcome to the NuCalm private access site. This site is used to engage with current NuCalm lifetime users. We want to know how we can develop and deliver technologies that help you live better lives. To do that, we need to share ideas, prototypes, and new advancements so we can get your feedback. We appreciate you and are honored to serve you. Let's do great things together!</h3>
+    <h3 className="subHero">Welcome to the NuCalm® private access site. This site is used to engage with current NuCalm® lifetime users. We want to know how we can develop and deliver technologies that help you live better lives. To do that, we need to share ideas, prototypes, and new advancements so we can get your feedback. We appreciate you and are honored to serve you. Let's do great things together!</h3>
     <div className="buttonDiv">
     <Link to="/beta">      
       <button className="loginButton" to="/app/beta">
